63b86171ea29423001d2dcc01a1a6349f3a9f86f
gheezl <gheezl25@gmail.com>
2020-10-26 17:00:59 -0400

    Added a backgroud component

---
+++ src/components/background/background.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react"
+
+import "./header.css"
+
+const Background = () => {
+    return (
+        <Fragment>
+            <div>
+                <img src="" />
+            </div>
+        </Fragment>
+    )
+}
