dd353ba733aa40bd8e77f116221619b8eb656cdd
gheezl <gheezl25@gmail.com>
2020-10-26 18:33:26 -0400

    Added a background image

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css"
 
 import Header from "./components/header/header.jsx"
 import Display from "./components/display/display.jsx"
+import Background from "./components/background/background.jsx"
 
 
 const App = () => {
   return (
     <Fragment>
+      <Background />
       <Header />
       <Display />
     </Fragment>
--- src/components/background/background.jsx
+++ src/components/background/background.jsx
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react"
 
-import "./header.css"
+import "./background.css"
 
 const Background = () => {
     return (
         <Fragment>
-            <div>
-                <img src="" />
+            <div className="image-border">
+                <img src="https://media1.giphy.com/media/UYBDCJjwOd9Re/giphy.gif?cid=ecf05e477fcodumizifb0qeqak6qvzyy7cba0nfqfjtg07qn&rid=giphy.gif" width="100%" />
             </div>
         </Fragment>
     )
 }
+
+export default Background;
